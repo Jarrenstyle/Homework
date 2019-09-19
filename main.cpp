5ffc0f613d59c4726435d01afed496b23bcc3567
Jarrenstyle <55443245+Jarrenstyle@users.noreply.github.com>
2019-09-19 08:39:21 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+// Name: Jarren Style 
+// Intro To CS 
+// Period 1
+// Jarren Style 
+// 2019-09-19 
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Name:Jarren Style" << std::end1
+		<< "Period 1: Computer Science" << std::end1
+		<< "Period 2: Earth Science" << std::end1
+		<< "Period 3: House" << std::end1
+		<< "Period 4: Health" << std:end1
+		<< "Period 5: Geometry" << std::end1
+		<< "Period 6: English Honors" << std::end1
+		<< "Period 7: Lunch" << std::end1
+		<<"Period 8: Spanish 1"<<std::end1
+		<<"Period 9: Gym"<< std::end1
+		<<"Period 10: Global History"<< std::end1
+}
