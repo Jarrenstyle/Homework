57263564595eae2461f3d1f1f3823df2ba78e218
Jarrenstyle <55443245+Jarrenstyle@users.noreply.github.com>
2019-11-12 18:14:56 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,27 +1,15 @@
-#include <iostream> 
-#include <cmath>
-// C++ program to find the area of Pentagon 
-#include<bits/stdc++.h> 
-using namespace std; 
-  
-// Function to find area of pentagon 
-float findArea(float a) 
-{ 
-    float area; 
-    
-    // Formula to find area 
-    area = (sqrt(5 * (5 + 2 * (sqrt(5)))) * a * a) / 4; 
-    
-    return area; 
-} 
-// Driver code 
-int main() 
-{ 
-    float a = 5; 
-      
-    // function calling 
-    cout << "Area of Pentagon: " << findArea(a); 
-      
-    return 0; 
+#include <iostream>
+using namespace std;
 
+ int main() {
+   float radius, area_circle;
+
+   // take radius as input
+   cout << "Enter the radius of circle: ";
+   cin >> radius;
+
+   area_circle = 3.14 * radius * radius;
+   cout << "Area of circle: " << area_circle << endl;
+
+   return 0;
 }
